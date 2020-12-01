3b3eaa463e160fe25cbf7aef3e04da7bab4569ac
Reykjavik151 <Reykjavik151@gmail.com>
2020-12-01 18:11:29 +0200

    feat: added all of Poppins fontFamily

--- app/utils/fonts.ts
+++ app/utils/fonts.ts
@@ -1,4 +1,12 @@
 export const FONTS = {
+  POPPINS_BLACK: 'Poppins-Black',
+  POPPINS_THIN: 'Poppins-Thin',
+  POPPINS_200: 'Poppins-ExtraLight',
+  POPPINS_300: 'Poppins-Light',
   POPPINS_REGULAR: 'Poppins-Regular',
+  POPPINS_500: 'Poppins-Medium',
+  POPPINS_600: 'Poppins-SemiBold',
+  POPPINS_BOLD: 'Poppins-Bold',
+  POPPINS_EXTRA_BOLD: 'Poppins-ExtraBold',
   SF_PRO_DISPLAY_REGULAR: 'SFProDisplay-Regular',
 };
