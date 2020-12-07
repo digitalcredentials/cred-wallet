bed6c30a9304e0201dc0e704e4c9b3790047d517
Artem <arte.tkachuk@gmail.com>
2020-12-07 12:19:48 +0200

    added did key generation

--- App.tsx
+++ App.tsx
@@ -1,3 +1,7 @@
+import process from 'process';
+import buffer from 'buffer';
+global.Buffer = buffer.Buffer;
+
 import 'react-native-gesture-handler';
 import React, { FunctionComponent } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -5,6 +9,8 @@ import { enableScreens } from 'react-native-screens';
 import { Provider } from 'react-redux';
 import RootNavigator from './app/navigation/root-navigator';
 import { store } from './app/redux';
+
+import './shim';
 
 enableScreens();
 
