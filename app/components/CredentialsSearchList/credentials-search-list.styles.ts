b2355ff7cb276fc7f9fc0d867d7b09551051570d
Reykjavik151 <Reykjavik151@gmail.com>
2020-12-08 17:34:10 +0200

    feat: added No Result for search

--- app/components/CredentialsSearchList/credentials-search-list.styles.ts
+++ app/components/CredentialsSearchList/credentials-search-list.styles.ts
@@ -1,12 +1,38 @@
-import { ViewStyle, StyleSheet } from 'react-native';
+import { ViewStyle, StyleSheet, TextStyle } from 'react-native';
+import { COLORS } from '../../utils/colors';
+import { FONTS } from '../../utils/fonts';
 
 interface IssuersListStyles {
   container: ViewStyle;
+  noResultsContainer: ViewStyle;
+  noResultsIconContainer: TextStyle;
+  noResultsTitle: TextStyle;
+  noResultsSubtitle: TextStyle;
 }
 
 export const styles: IssuersListStyles = StyleSheet.create<IssuersListStyles>({
   container: {
     flex: 1,
     padding: 16,
+  },
+  noResultsContainer: {
+    flex: 1,
+    paddingTop: 74,
+    alignItems: 'center',
+  },
+  noResultsIconContainer: {
+    marginBottom: 30,
+  },
+  noResultsTitle: {
+    marginBottom: 10,
+    fontFamily: FONTS.POPPINS_500,
+    fontSize: 17,
+    lineHeight: 25.5,
+  },
+  noResultsSubtitle: {
+    fontFamily: FONTS.POPPINS_300,
+    color: COLORS.SILVER,
+    fontSize: 11,
+    lineHeight: 14,
   },
 });
--- app/components/CredentialsSearchList/index.tsx
+++ app/components/CredentialsSearchList/index.tsx
@@ -1,11 +1,14 @@
 import React, { useCallback } from 'react';
 import { FlatList, View } from 'react-native';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
 import { styles } from './credentials-search-list.styles';
 import { CredentialsSearchListProps } from './credentials-search-list.props';
 import { useCredentials } from '../../redux/certificates';
 import { useKeyExtractor } from '../../utils/hooks';
 import { ICredentials } from '../../utils/types';
+import { COLORS } from '../../utils/colors';
+import { Text } from '../Text';
 
 export const CredentialsSearchList: React.FunctionComponent<CredentialsSearchListProps> = () => {
   const credentials = useCredentials();
@@ -17,7 +20,7 @@ export const CredentialsSearchList: React.FunctionComponent<CredentialsSearchLis
 
   const keyExtractor = useKeyExtractor<ICredentials>('credential-search');
 
-  return (
+  return credentials.length ? (
     <FlatList
       data={credentials}
       renderItem={renderItem}
@@ -25,5 +28,15 @@ export const CredentialsSearchList: React.FunctionComponent<CredentialsSearchLis
       showsVerticalScrollIndicator={false}
       style={styles.container}
     />
+  ) : (
+    <View style={styles.noResultsContainer}>
+      <View style={styles.noResultsIconContainer}>
+        <FontAwesomeIcon name="search" size={70} color={COLORS.SILVER} />
+      </View>
+      <Text style={styles.noResultsTitle}>No results</Text>
+      <Text style={styles.noResultsSubtitle}>
+        Change the request and try again
+      </Text>
+    </View>
   );
 };
