12653088c1d17faebe4d37aefa63777fd29c7044
Reykjavik151 <Reykjavik151@gmail.com>
2020-12-14 13:30:39 +0200

    feat: useMount hook

--- app/utils/hooks.ts
+++ app/utils/hooks.ts
@@ -1,5 +1,7 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import { IWithID } from './types';
+
+export const useMount = (func: () => void) => useEffect(func, []);
 
 export const useKeyExtractor = <TItem extends IWithID>(prefix: string) =>
   useCallback(
