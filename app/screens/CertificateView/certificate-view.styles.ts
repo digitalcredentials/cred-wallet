7bbe52451ffde44afd8dcfc06a9f566ea7042179
Reykjavik151 <Reykjavik151@gmail.com>
2021-05-21 13:21:03 +0300

    refactor: CertificateView screen

--- app/screens/CertificateView/certificate-view.styles.ts
+++ app/screens/CertificateView/certificate-view.styles.ts
@@ -1,13 +1,14 @@
 import { StyleSheet, ViewStyle } from 'react-native';
+
 import { COLORS } from '../../utils/colors';
 
-interface CertificateViewScreenStyles {
+interface ICertificateViewScreenStyles {
   container: ViewStyle;
   contentContainer: ViewStyle;
   flexContainer: ViewStyle;
 }
 
-export const styles = StyleSheet.create<CertificateViewScreenStyles>({
+export const styles = StyleSheet.create<ICertificateViewScreenStyles>({
   container: {
     flex: 1,
     backgroundColor: COLORS.GRAY_TRANSPARENT_BACKGROUND,
