721aead1c33b808ab3931fe2c6d6b9ab91373491
Reykjavik151 <Reykjavik151@gmail.com>
2021-05-21 13:31:37 +0300

    refactor: Loading screen

--- app/screens/Loading/loading.props.ts
+++ app/screens/Loading/loading.props.ts
@@ -1,7 +1,8 @@
 import { StackScreenProps, StackNavigationProp } from '@react-navigation/stack';
+
 import { RootParams } from '../../navigation/root-navigator';
 
 type NavigationProps = StackNavigationProp<RootParams, 'Loading'>;
 type RouteProps = StackScreenProps<RootParams, 'Loading'>;
 
-export interface LoadingScreenProps extends NavigationProps, RouteProps {}
+export interface ILoadingScreenProps extends NavigationProps, RouteProps {}
