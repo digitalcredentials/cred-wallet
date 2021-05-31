c690de9a7ae7895e5032c33b388d5013e749f105
Dirk Uys <dirkcuys@gmail.com>
2021-05-31 11:41:31 +0200

    Add version to about screen

--- app/screens/About/index.tsx
+++ app/screens/About/index.tsx
@@ -4,6 +4,7 @@ import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
 import { IAboutScreenProps } from './about.props';
 import { styles } from './about.styles';
 import { SettingsHeader } from '../../components';
+import { name as app_name, version as app_version }  from '../../../package.json';
 
 export const AboutScreen: React.FC<IAboutScreenProps> = ({
   navigation,
@@ -12,6 +13,9 @@ export const AboutScreen: React.FC<IAboutScreenProps> = ({
     <View style={styles.container}>
       <SettingsHeader title="About" />
       <View style={styles.contentContainer}>
+        <Text>
+          { app_name } { app_version }
+        </Text>
         <Text>
           This mobile wallet was developed by the Digital Credentials Consortium, a network of leading international universities designing an open infrastructure for academic credentials.
         </Text>
